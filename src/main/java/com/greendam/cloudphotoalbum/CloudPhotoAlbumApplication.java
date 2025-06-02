678d306177653df66ed666f89d0f4d029ba95e45
ForeverGreenDam <2985601245@qq.com>
2025-06-02 16:24:03 +0800

    初始化

---
+++ src/main/java/com/greendam/cloudphotoalbum/CloudPhotoAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.greendam.cloudphotoalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPhotoAlbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPhotoAlbumApplication.class, args);
+    }
+
+}
